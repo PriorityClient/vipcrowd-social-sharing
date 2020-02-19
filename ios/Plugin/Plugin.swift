87fda891459b67ab68956a66f6b1760b10debff8
Pat Murray <pmurpmur@gmail.com>
2020-02-19 09:31:47 -0600

    test

--- ios/Plugin/Plugin.swift
+++ ios/Plugin/Plugin.swift
@@ -1,5 +1,6 @@
 import Foundation
 import Capacitor
+import FBSDKShareKit
 
 /**
  * Please read the Capacitor iOS Plugin Development Guide
@@ -9,9 +10,24 @@ import Capacitor
 public class SocialShare: CAPPlugin {
 
     @objc func echo(_ call: CAPPluginCall) {
-        let value = call.getString("value") ?? ""
-        call.success([
-            "value": value
-        ])
+        let url = call.getString("url") ?? ""
+        let quote = call.getString("quote") ?? ""
+        
+        let shareContent = ShareLinkContent()
+        shareContent.contentURL = URL.init(string: url)!
+        shareContent.quote = quote
+        let shareDialog = ShareDialog()
+        shareDialog.shareContent = shareContent
+        shareDialog.mode = .automatic
+        
+        do {
+            try shareDialog.show()
+            call.resolve([
+                url: url,
+                quote: quote
+            ])
+        } catch let error {
+            call.reject("FB failure", error)
+        }
     }
 }
